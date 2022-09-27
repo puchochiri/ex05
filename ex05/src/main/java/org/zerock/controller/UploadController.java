d37007363eecb1dbd45609299c2b8aec96bb4391
양선우 <@DESKTOP-94O1RKP>
2022-09-27 18:09:08 +0900

    20220927 commit

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -2,6 +2,8 @@ package org.zerock.controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,13 +11,18 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.AttachFileDTO;
@@ -171,5 +178,103 @@ public class UploadController {
 		return false;
 	}
 	
+	//http://localhost:9057/display?fileName=2022/09/27/꽃.jpg
+	@GetMapping("/display")
+	 @ResponseBody
+	 public ResponseEntity<byte[]> getFile(String fileName) {
+		 
+		 log.info("fileName: " + fileName);
+		 
+		 File file = new File("c:\\upload\\" + fileName);
+		 
+		 log.info("file: " + file);
+		 
+		 ResponseEntity<byte[]> result = null;
+		 
+		 try {
+			 HttpHeaders header = new HttpHeaders();
+			 
+			 header.add("Content-Type", Files.probeContentType(file.toPath()));
+			 result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file),header,HttpStatus.OK);
+			 
+			 
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		 return result;
+		 
+		 
+	 }
+	 
+	 @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	 @ResponseBody
+	 public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent")
+	 String userAgent, String fileName) {
+		 
+		 log.info("download file: " + fileName);
+		 
+		 Resource resource = new FileSystemResource("c:\\upload\\" + fileName);
+		 
+		 log.info("resource: " + resource);
+		 
+		 if(resource.exists() == false) {
+			 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		 }
+		 
+		 String resourceName = resource.getFilename();
+		 
+		 // remove UUID
+		 String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+		 
+		 HttpHeaders headers = new HttpHeaders();
+		 
+		 try {
+			 String downloadName = null;
+			 
+			 if(userAgent.contains("Trident")) {
+				 log.info("IE browser");
+				 
+				 downloadName = URLEncoder.encode(resourceOriginalName,"UTF-8").replaceAll("\\+", " ");
+				 
+			 } else if(userAgent.contains("Edge")) {
+				 
+				 log.info("Edge browser");
+				 
+				 downloadName = URLEncoder.encode(resourceOriginalName,"UTF-8");
+				 
+				 log.info("Edge Name: " + downloadName);
+				 
+			 } else {
+				 log.info("Chrome browser");
+				 
+				 downloadName = new String(resourceOriginalName.getBytes("UTF-8"),"ISO-8859-1");
+				 
+			 }
+			 log.info("downloadName: " + downloadName);
+			 
+			 headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+			 
+			 
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		 
+		 return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	 }
+	 
+	 @PostMapping("/deleteFile")
+	 @ResponseBody
+	 public ResponseEntity<String> deleteFile(String fileName, String type) {
+		
+		 log.info("deleteFile: " + fileName);
+		 
+		 File file;
+		return null;
+	 }
+		
 
 }
